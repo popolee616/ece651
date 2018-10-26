0769ce08a3b1a101eeb88f3edc73cdd00abf836a
popolee616 <x824li@uwaterloo.ca>
2018-10-26 18:04:10 -0400

    Server dealing POST request and accept json data

--- Server/main.py
+++ Server/main.py
@@ -7,7 +7,15 @@ app = Flask(__name__)
 def hello():
     return "Hello World"
 
+@app.route('/register',methods=['POST'])
+def register():
+    content = request.json
+
+    print(content['userName'])
+    print(content['userPassword'])
+    print(content['userEmail'])
+    return "Register successfully!\t Welcome " + content['userName']
 #running web app in local machine
 if __name__=='__main__':
+    #http://127.0.0.1:5000/
     app.run(host='0.0.0.0', port=5000)
-	#One can use either 127.0.0.1 or localhost ip to access this webpage
